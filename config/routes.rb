af7e54d6bf50f6afe2ce191ca1824678686aca3f
KAKUTANI Shintaro <shintaro@kakutani.com>
2008-08-15 00:14:57 +0900

    add some useful(to me) links.

--- config/routes.rb
+++ config/routes.rb
@@ -38,7 +38,7 @@ ActionController::Routing::Routes.draw do |map|
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
   map.root :controller => "events"
-  map.connect ':name/:action', :controller => 'events', :action => 'show'
+  map.event':name/:action', :controller => 'events', :action => 'show'
 
   # See how all your routes lay out with "rake routes"
 
