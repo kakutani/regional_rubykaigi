d0ede029c418e0ece5eb16fd84608285d19e805f
KAKUTANI Shintaro <shintaro@kakutani.com>
2008-08-30 10:26:29 +0900

    simple user admin screens.

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,48 @@
+class Admin::UsersController < AdminController
+  def index
+    @users = User.find(:all)
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(params[:user])
+
+    respond_to do |format|
+      if @user.save
+        flash[:notice] = 'User was successfully created.'
+        format.html { redirect_to(:action => 'index') }
+      else
+        format.html { render :action => "new" }
+      end
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    respond_to do |format|
+      if @user.update_attributes(params[:user])
+        flash[:notice] = 'User was successfully updated.'
+        format.html { redirect_to(@user) }
+      else
+        format.html { render :action => "edit" }
+      end
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    respond_to do |format|
+      format.html { redirect_to(admin_users_url) }
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,13 @@
 ActionController::Routing::Routes.draw do |map|
-# FIXME temporary disabled -->
   map.logout '/logout', :controller => 'sessions', :action => 'destroy'
   map.login '/login', :controller => 'sessions', :action => 'new'
-#  map.register '/register', :controller => 'users', :action => 'create'
-#  map.signup '/signup', :controller => 'users', :action => 'new'
-#  map.activate '/activate/:activation_code', :controller => 'users', :action => 'activate', :activation_code => nil
-#  map.resources :users
-#
   map.resource :session
-# <-- FIXME temporary disabled
 
   map.namespace(:admin) do |admin|
-	admin.resources :events do |event|
+    admin.resources :events do |event|
       event.resources :attendees
     end
+    admin.resources :users
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
