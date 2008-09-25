63f355761a3dbcd3503b0652e51e66943eb9c897
KAKUTANI Shintaro <shintaro@kakutani.com>
2008-09-25 17:27:23 +0900

    fixed bug: an attendee using same email address can attend multiple events. thanks to snoozer-05 for report the bug :-)

--- app/models/attendee.rb
+++ app/models/attendee.rb
@@ -2,5 +2,5 @@ class Attendee < ActiveRecord::Base
   belongs_to :event
 
   validates_presence_of :name, :email
-  validates_uniqueness_of :email
+  validates_uniqueness_of :email, :scope => :event_id
 end
--- spec/models/attendee_spec.rb
+++ spec/models/attendee_spec.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Attendee do
@@ -8,4 +9,17 @@ describe Attendee do
   it "should_not be valid" do
     @attendee.should_not be_valid
   end
+end
+
+describe Attendee do
+  before do
+    Attendee.delete(:all)
+    alice_attended_past_event = Attendee.create(:event_id => 1, :email => 'alice@example.com', :name => 'Alice')
+    @alice = Attendee.new(:event_id => 2, :email => 'alice@example.com', :name => 'Alice')
+  end
+
+  it "参加者は同一メールアドレスで他のイベントに参加できること" do
+    @alice.should be_valid
+  end
+
 end
