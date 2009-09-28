c7f62e052d62a2fc41c8785e7bd6f4bff5c416f4
KAKUTANI Shintaro <shintaro@kakutani.com>
2009-09-28 19:19:31 +0900

    change attendees listing order to created_at desc.

--- app/controllers/admin/attendees_controller.rb
+++ app/controllers/admin/attendees_controller.rb
@@ -2,7 +2,7 @@ class Admin::AttendeesController < AdminController
   before_filter :fetch_event_with_attendees
 
   def fetch_event_with_attendees
-    @event = Event.find(params[:event_id], :include => :attendees, :order => "attendees.name")
+    @event = Event.find(params[:event_id], :include => :attendees, :order => "attendees.created_at DESC")
   end
 
   def index
