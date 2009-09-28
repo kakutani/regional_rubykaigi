0e3ab3e0ac9aab86d88eb7367897f9395a9d38a8
KAKUTANI Shintaro <shintaro@kakutani.com>
2009-09-28 18:38:58 +0900

    update to Rails 2.3.4.
    (prototype.jsはアップデートしていない)

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.1.1' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -33,7 +33,7 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem 'haml'
-  config.gem 'ruby-openid', :lib => 'openid', :version => '>= 2.0.4'
+  config.gem 'ruby-openid', :lib => 'openid'
 
   # Only load the plugins named here, in the order given. By default, all plugins
   # in vendor/plugins are loaded in alphabetical order.
